a9189ec2715ee3838ed71aaa28d747cf2e768edc
torresashjian <migueltorreslopez@gmail.com>
2021-02-25 09:54:32 -0800

    Add setup script

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,15 +2,19 @@
 'use strict';
 
 const fs = require('fs');
+const k8s = require('@kubernetes/client-node');
 
 const configFileNames = ['proxy.conf.prod.au.json','proxy.conf.prod.eu.json', 'proxy.conf.prod.us.json'];
-const minikubeAPIURL = "http://localhost:8080/api/"
 const edgeXRemoteGatewayURL = '/edgex/remotegateway/*';
 const airEndpointURL = '/airEndpoint/*';
 
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
 function setupAir() {
     var infraType = process.argv.slice(2);
-    if (infraType ==='minikube'){
+    if (infraType == 'minikube'){
         setupAirMinikube();
     }
     
@@ -18,6 +22,21 @@ function setupAir() {
 };
 
 function setupAirMinikube() {
+    /*k8sApi.listEndpointsForAllNamespaces().then((res) => {
+        console.log(res);
+    });
+    k8sApi.listServiceForAllNamespaces().then((res) => {
+        var items = res.body.items;
+        items.forEach(serviceItem => {
+                if (serviceItem.metadata.name == 'core-air-cors-anywhere-service'){
+                    console.log(serviceItem.status.loadBalance);
+                } else if (serviceItem.metadata.name == 'ingress-nginx-controller-admission'){
+                    console.log(serviceItem.status.loadBalancer);
+                }   
+            }
+        );
+    });
+
     // Setup files
     configFileNames.forEach(configFileName => {
         console.log("Updating config file: "+configFileName);
@@ -26,7 +45,7 @@ function setupAirMinikube() {
         proxyjson[edgeXRemoteGatewayURL].target = "CHANGED!!!!!";
         let data = JSON.stringify(proxyjson, null, 2);
         fs.writeFileSync(configFileName, data);
-    });
+    });*/
 }
 
 setupAir()
