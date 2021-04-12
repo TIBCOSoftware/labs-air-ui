b4031e3edb1b8383acf6a0bb4b52d88544a43051
marcelo <gallardo@tibco.com>
2021-04-12 11:21:00 -0400

    Changed name of input and output ports

--- src/app/components/rete/components/notification-pipe-component.ts
+++ src/app/components/rete/components/notification-pipe-component.ts
@@ -9,10 +9,10 @@ export class NotificationPipeComponent extends Component {
     }
 
     builder(node) {
-        const inp1 = new Input("evt1", "Event1", eventSocket);
-        const out = new Output("event", "Event", eventSocket);
+        const inp1 = new Input("evt1", "Event", eventSocket);
+        const out = new Output("event", "Notify Event", eventSocket);
         // const err = new Output("error", "Error", errorSocket);
-        const control = new ImageControl(this.editor, "event", "device.png");
+        const control = new ImageControl(this.editor, "event", "Icon_Pipeline.svg");
 
         return node.addControl(control)
             .addInput(inp1)
