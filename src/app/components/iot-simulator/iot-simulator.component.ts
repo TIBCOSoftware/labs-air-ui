d92e5216715c26d3f9bb0f06d9329552c39f30fe
tibco-tdinh <tdinh@tibco.com>
2021-04-22 15:37:54 -0500

    added HTTP request support and styling for simulator

--- src/app/components/iot-simulator/iot-simulator.component.ts
+++ src/app/components/iot-simulator/iot-simulator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { request } from 'http';
 
 @Component({
   selector: 'app-iot-simulator',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IotSimulatorComponent implements OnInit {
 
-  constructor() { }
+  scanURL = "";
+  cameraURL = "";
+  scaleURL = "";
+  openCartURL = "";
+  closeCartURL = ""
+  paymentURL = ""
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  sendRequest(whichURL: string, requestBody: string) {
+    return this.http.post(whichURL, JSON.stringify(requestBody)).subscribe(response => {
+      console.log(response);
+    })
   }
 
 }
