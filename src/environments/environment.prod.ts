ff814aa0d549f74cfd2827ce7550231aea53d3b9
Syrine Matoussi <smatoussi@Syrines-MacBook-Pro.local>
2021-06-29 17:22:04 -0400

    removed proxy.conf.prod.us.js and added urls in enviroment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  dgraphUrl: 'http://192.168.99.100:31417'
+  dgraphUrl: 'http://192.168.99.102:30779',
+  f1EndpointUrl: 'http://54.81.13.248:5408',
+  airEndpointUrl:'http://a77d5e583ea66440bb0e26be76aec6bf-1483924641.us-west-2.elb.amazonaws.com',
+  remoteGatewayUrl: 'http://localhost:8043'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
-  dgraphUrl: 'http://192.168.99.102:30779'
+  dgraphUrl: 'http://192.168.99.102:30779',
+  f1EndpointUrl: 'http://54.81.13.248:5408',
+  airEndpointUrl:'http://a77d5e583ea66440bb0e26be76aec6bf-1483924641.us-west-2.elb.amazonaws.com',
+  remoteGatewayUrl: 'http://localhost:8043'
 };
 
 /*
